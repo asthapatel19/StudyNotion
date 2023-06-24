ee9a6d3f1bea75b99c81c1a7a5da75b84b4b1ebc
asthapatel19 <astha.p@ahduni.edu.in>
2023-06-24 17:19:58 +0530

    courseDetails added

--- server/controllers/Course.js
+++ server/controllers/Course.js
@@ -117,3 +117,52 @@ exports.showAllCourses = async (req,res) =>{
         })
     }
 }
+
+//get Course Details
+exports.getCourseDetails = async (req,res)=>{
+    try{
+        //get courseId
+        const {courseId} = req.body;
+
+        //find course details
+        const courseDetails = await Course.find({_id:courseId})
+        .populate(
+            {
+                path:"instructor",
+                populate:{
+                    path:"additionalDetails",
+                },
+            })
+            .populate("category")
+            .populate("ratingAndreviews")
+            .populate({
+                path:"courseContent",
+                populate:{
+                    path:"subSection",
+                },
+            })
+            .exec();
+
+        //validate
+        if(!courseDetails){
+            return res.status(400).json({
+                success:false,
+                message:`Could not find the course with ${courseId}`,
+            })
+        }
+
+        //return res
+        return res.status(200).json({
+            success:true,
+            message:"Course details fetched successfully",
+            data:courseDetails
+        })
+
+    }catch(error){
+        console.log(error)
+        return res.status(500).json({
+            success:false,
+            message:error.message,
+        })
+    }
+}
