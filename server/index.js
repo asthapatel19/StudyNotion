726d6fafb89529a67ecbec9e51b8d6de39af39e2
asthapatel19 <astha.p@ahduni.edu.in>
2023-06-25 11:17:34 +0530

    user-routes added

--- server/index.js
+++ server/index.js
@@ -1,58 +1,61 @@
 const express = require("express");
 const app = express();
 
-const userRoutes = require("./routes/User")
-const profileRoutes = require("./routes/Profile")
-const paymentRoutes = require("./routes/Payment")
-const courseRoutes = require("./routes/Course")
+const userRoutes = require("./routes/User");
+const profileRoutes = require("./routes/Profile");
+const paymentRoutes = require("./routes/Payments");
+const courseRoutes = require("./routes/Course");
 
-const database = require('./config/database')
-const cookieParser = require("cokkie-parser")
-const cors = require("cors")
-const {cloudinaryConnect} = require('./config/cloudinary')
-const fileUpload = require("express-fileuload")
-const dotenv = require("dotenv")
+const database = require("./config/database");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const {cloudinaryConnect } = require("./config/cloudinary");
+const fileUpload = require("express-fileupload");
+const dotenv = require("dotenv");
 
-dotenv.config()
+dotenv.config();
 const PORT = process.env.PORT || 4000;
 
-database.connect()
+//database connect
+database.connect();
 
 //middlewares
-app.use(express.json())
-app.use(cookieParser())
+app.use(express.json());
+app.use(cookieParser());
 
 //cors - to entertain req from frontend
 app.use(
-    cors({
-        origin:"http://localhost:3000",
-        credentials:true,
-    })
+	cors({
+		origin:"http://localhost:3000",
+		credentials:true,
+	})
 )
 
-//cloudinary connection
 app.use(
-    fileUpload({
-        useTempFiles:true,
-        tempFileDir:"/tmp"
-    })
+	fileUpload({
+		useTempFiles:true,
+		tempFileDir:"/tmp",
+	})
 )
+//cloudinary connection
+cloudinaryConnect();
 
 //routes
-app.use("/api/vi/auth",userRoutes)
-app.use("/api/vi/profile",profileRoutes)
-app.use("/api/vi/course",courseRoutes)
-app.use("/api/vi/payment",paymentRoutes)
+app.use("/api/v1/auth", userRoutes);
+app.use("/api/v1/profile", profileRoutes);
+app.use("/api/v1/course", courseRoutes);
+app.use("/api/v1/payment", paymentRoutes);
+
 
 //default route
-app.get("/", (req,res)=>{
-    return res.json({
-        success:true,
-        message:'Your server is up and running..'
-    })
+app.get("/", (req, res) => {
+	return res.json({
+		success:true,
+		message:'Your server is up and running....'
+	});
+});
+
+app.listen(PORT, () => {
+	console.log(`App is running at ${PORT}`)
 })
 
-//activate server
-app.listen(PORT, ()=>{
-    console.log(`App is running at ${PORT}`)
-})
--- server/routes/User.js
+++ server/routes/User.js
@@ -2,7 +2,7 @@ const express = require("express")
 const router = express.Router()
 
 // Importing the required controllers and middleware functions
-const {login,signup,sendotp,changePassword} = require("../controllers/Auth")
+const {logIn,signUp,sendOTP,changePassword} = require("../controllers/Auth")
 const {resetPasswordToken,resetPassword} = require("../controllers/ResetPassword")
 const { auth } = require("../middlewares/auth")
 
@@ -13,13 +13,13 @@ const { auth } = require("../middlewares/auth")
 // ********************************************************************************************************
 
 // Route for user login
-router.post("/login", login)
+router.post("/login", logIn)
 
 // Route for user signup
-router.post("/signup", signup)
+router.post("/signup", signUp)
 
 // Route for sending OTP to the user's email
-router.post("/sendotp", sendotp)
+router.post("/sendotp", sendOTP)
 
 // Route for Changing the password
 router.post("/changepassword", auth, changePassword)
