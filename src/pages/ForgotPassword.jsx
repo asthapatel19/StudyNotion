5ca2fa30150212760cd45dc653740d582598f649
asthapatel19 <astha.p@ahduni.edu.in>
2023-08-08 18:05:25 +0530

    first commit

--- src/pages/ForgotPassword.jsx
+++ src/pages/ForgotPassword.jsx
@@ -1,9 +1,58 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useSelector } from 'react-redux'
 
 export const ForgotPassword = () => {
+
+    const [emailSent,setEmailSent] = useState(false)
+    const [email,setEmail] = useState("")
+    const {loading} = useSelector((state)=>state.auth)
+
   return (
     <div className=''>
-        
+        {
+            loading ? (
+
+                <div>Loading...</div>
+
+            ) : (
+                <div>
+
+                    <h1>
+                        {
+                            !emailSent ? "Reset your password" : "Check your Email"
+                        }
+                    </h1>
+
+                    <p>
+                        {
+                            !emailSent ? "Have no fear. We'll email you instructions to reset your password. If you dont have access to your email we can try account recovery"
+                            : `We have sent the reset email to ${email}`
+                        }
+                    </p>
+
+                    <form>
+                        {
+                            !emailSent && (
+                                <label>
+                                    <p>Email Address*</p>
+
+                                    <input
+                                      required
+                                      type='email'
+                                      name='email'
+                                      value={email}
+                                      onChange={(e)=>setEmail(e.target.value)}
+                                      placeholder='Enter your email address'
+                                    />
+
+                                </label>
+                            )
+                        }
+                    </form>
+
+                </div>
+            )
+        }
     </div>
   )
 }
